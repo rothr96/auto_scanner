f73d5279cc830c63bfd6cd2d8fce660815505414
rothr96 <rothr96@gmail.com>
2021-12-26 19:59:23 +0200

    muuta kayttamaan gmailia

--- auto_scanner/email.py
+++ auto_scanner/email.py
@@ -11,9 +11,9 @@ def send_email(passwd: str, msgs: List[str]) -> None:
     text['Subject'] = 'Notifications from auto scanner'
     text['From'] = FROM_ADDRESS
     text['To'] = TO_ADDRESS
-    # session = smtplib.SMTP('smtp.gmail.com', 587)
-    # session.starttls() #enable security
-    # session.login(FROM_ADDRESS, passwd)
-    session = smtplib.SMTP('localhost', 1025)
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls() #enable security
+    session.login(FROM_ADDRESS, passwd)
+    # session = smtplib.SMTP('localhost', 1025)
     session.sendmail(FROM_ADDRESS, [TO_ADDRESS], text.as_string())
     session.quit()
